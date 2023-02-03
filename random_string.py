f418dad98c3ad2db94336267fa05d378c9f1346b
Julian de Almeida Santos <julian.santos.info@gmail.com>
2023-02-03 15:30:49 -0300

    Atualizado

--- random_string.py
+++ random_string.py
@@ -4,15 +4,14 @@ import string
 import time
 import datetime
 
-dir_path = "D:\\Documents\\Python\ChatGPT\\"
-filename = datetime.datetime.now().isoformat().replace(":","").replace("-","").replace(".","")
-filename = os.path.join(dir_path, f"minha_lista_{filename}.txt")
-
 def random_string(stringLength=600):
     letters = string.ascii_letters + string.digits
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 def main():
+    dir_path = "D:\\Documents\\Python\ChatGPT\\"
+    filename = datetime.datetime.now().isoformat().replace(":","").replace("-","").replace(".","")
+    filename = os.path.join(dir_path, f"minha_lista_{filename}.txt")
     with open(filename, "w") as f:
         f.write(random_string())
     os.system(f'git add .')
